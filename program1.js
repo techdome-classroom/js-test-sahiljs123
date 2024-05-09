190d30f44639ac58eae51272a783fe414c95d3b7
sahiljs123 <162563976+sahiljs123@users.noreply.github.com>
2024-05-09 13:02:41 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,9 +2,28 @@ const getTotalIsles = function (grid) {
 
 
     // write your code here
+    let count=0;
+    let flag=true;
+    for (let i = 0; i < grid.length; i++) {
+      
+      
+      for (let index = 0; index < grid[i].length; index++) {
+        const element = gris[index];
+        if(flag && arr[i][index]=="L"){
+            count++;
+            flag=false;
+        }
+        if(element=="L"){
+          if(grid[i][index]==grid[i][index+1]){
+            grid[index]="W"
+          }
+        }
+        
+      }
+    }
+    return count;
 
   };
   
   module.exports = getTotalIsles;
-
 
